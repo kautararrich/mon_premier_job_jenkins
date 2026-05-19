d6252d382bd32fac74329d929a6b68cfa88d03a4
kautar <kautararrich@gmail.com>
2026-05-19 12:36:13 +0100

    first commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+import sys
+
+print("=" * 40)
+print("bienvenue dans mon premier job jenkins")
+print("=" * 40)
+
+if len(sys.argv) > 1 :
+    nom = sys.argv[1]
+else:
+    nom = "etu jenkins"
+print(f"bonjour {nom}, ton job jenkins a reussi")
+
+a = 10
+b = 5
+print(f"{a}+{b}={a+b}")
+print(f"{a}-{b}={a-b}")
+
+assert a+b ==15
+
+print("ok")
+
